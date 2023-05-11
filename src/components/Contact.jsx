c118bd9c5a98f486237fd61fc3983e26ce632884
Christian Bosse <christianbosse1998@gmail.com>
2023-05-11 02:02:08 -0300

    Contact now working with 3D stars

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,8 +15,45 @@ const Contact = () => {
     });
 
     const [loading, setLoading] = useState(false);
-    const handleChange = (e) => {};
-    const handleSubmit = (e) => {};
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setForm({ ...form, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setLoading(true);
+        emailjs
+            .send(
+                "service_q5zsoiq",
+                "template_t7fbwcn",
+                {
+                    from_name: form.name,
+                    to_name: "Christian",
+                    from_email: form.email,
+                    to_email: "christianbosse1998@gmail.com",
+                    message: form.message,
+                },
+                "user_crG8QptGSQuA3QDSDiVJN"
+            )
+            .then(
+                () => {
+                    setLoading(false);
+                    alert(
+                        "Thanks you. I will get back to you as soon as possible!"
+                    );
+                    setForm({ name: "", email: "", message: "" });
+                },
+                (error) => {
+                    setLoading(false);
+                    console.log(error);
+                    alert("Something went wrong");
+                }
+            );
+    };
 
     return (
         <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
